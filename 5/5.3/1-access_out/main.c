3e946b5453d8b769b339a87629b251e972ede643
C <kangzinian@icloud.com>
2025-06-02 19:06:13 +0800

    数据、部分数组越界

---
+++ 5/5.3/1-access_out/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+// 数组访问越界
+int main(void) {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int j = 20;
+    int i = 10;
+    printf("a[0]=%d\n", arr[0]);
+    arr[5] = 6; //访问越界
+    arr[6] = 7;
+    for (int k = 1; k < i; k++) {
+        printf("a[%d]=%d\n", k, arr[k]);  // 访问越界
+    }
+    return 0;
+}
